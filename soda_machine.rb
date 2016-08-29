965d75220b1beb8596a18e554e25912b7d5228c3
Angelica Pilarca <email": null>
2016-08-29 15:00:04 -0700

    checkpoint_challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,20 @@ class SodaMachine
   end
 
   def current_inventory_count
+   return @sodas.length
   end
 
   def find_soda(soda_brand)
+    if @sodas.brand == soda_brand
+      return @sodas.soda_brand
+    # else
+    #   return nil
+    end
   end
 
   def sell(soda_brand)
+    @sodas.delete(soda_brand)
+    @cash = soda_brand[:price]
   end
 
 end
